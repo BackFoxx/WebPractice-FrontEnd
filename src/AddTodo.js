1057dd0f16fe415ea28b21d3343c22176555ff66
조승현 <jorealbusiness@gmail.com>
2021-12-12 19:20:54 +0900

    Add Todo 구현

---
+++ src/AddTodo.js
@@ -0,0 +1,65 @@
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+
+//사용자의 입력을 저장할 오브젝트
+class AddTodo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { item: { title: "" } }; 
+        this.add = props.add;
+    }
+
+    //1. State 연결 함수 작성
+    onInputChange = (e) => {
+        const thisItem = this.state.item;
+        thisItem.title = e.target.value;
+        this.setState({ item: thisItem });
+        console.log(thisItem);
+    }
+
+    //1. 버튼 함수 작성
+    onButttonClick = () => {
+        this.add(this.state.item); // add 함수 사용
+        this.setState({ item: {title: ""} });
+    }
+
+    //1. 엔터키 함수 작성
+    enterKeyEventHandler = (e) => {
+        if (e.key === 'Enter') {
+            this.onButttonClick();
+        }
+    }
+
+    render() {
+        //2. 함수 연결
+        return (
+            <Paper style={{ margin: 16, padding: 16 }}>
+                <Grid container>
+                    <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
+                        <TextField 
+                        placeholder="Add Todo here" 
+                        fullwidth
+                        onChange={this.onInputChange}
+                        //이벤트 핸들러 함수로 onInputChange를 연결한다.
+                        value={this.state.item.title}
+                        onKeyPress={this.enterKeyEventHandler} />
+                    </Grid>
+                    <Grid xs={1} md={1} item>
+                        <Button 
+                            fullwidth 
+                            color="secondary" 
+                            variant="outlined"
+                            onClick={this.onButttonClick}>
+                            +
+                        </Button>
+                    </Grid>
+                </Grid>
+            </Paper>
+        );
+    }
+}
+
+export default AddTodo;
--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 import Todo from "./Todo";
+import AddTodo from "./AddTodo.js";
 import List from "@material-ui/core/List";
 import Paper from "@material-ui/core/Paper";
+import Container from "@material-ui/core/Container";
 import "./App.css";
 
 
@@ -18,6 +20,16 @@ class App extends React.Component {
     };
   }
 
+  //1. 함수 추가
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length; // key를 위한 id 추가
+    item.done = false; // done 초기화
+    thisItems.push(item); // 리스트에 아이템 추가
+    this.setState({ items: thisItems }); //업데이트는 this.setState로 해준다
+    console.log("items : ",this.state.items);
+  }
+
   render() {
     var todoItems = this.state.items.length > 0 && (
       <Paper style={{ margin: 16 }}>
@@ -29,7 +41,14 @@ class App extends React.Component {
       </Paper>
     );
 
-    return <div className="App">{todoItems}</div>;
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add} />
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    )
   }
 }
 
